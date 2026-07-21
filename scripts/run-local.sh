9d8fdcd67f332002b405f470ecbcb087ba8cead3
lepsima <lepsima@proton.me>
2026-07-21 18:01:05 +0200

    script

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -2,8 +2,20 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+cd "$PROJECT_DIR"
+
+echo "Creating directories..."
+mkdir -p docker/config
+mkdir -p docker/data
+
+echo "Compiling..."
 cargo build --release
 
+echo "Building image..."
 docker build -t discord-bot .
 
-docker compose up
+echo "Initializing container..."
+docker compose up -d --force-recreate
